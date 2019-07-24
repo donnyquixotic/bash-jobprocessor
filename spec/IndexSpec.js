36c6ccd3c93dd8afa6ca7ccc7063b3f81a0e0230
Don Peat <don@caleos.io>
2019-07-24 09:12:31 -0700

    test: update describe for method in IndexSpec

--- spec/IndexSpec.js
+++ spec/IndexSpec.js
@@ -1,14 +1,14 @@
 //functions should be modularized & exported for testing, example purposes only
 let checkIfDepsCompleted = (arr, target) => target.every(v => arr.includes(v))
 
-describe("checker function", function() {
+describe("checkIfDepsCompleted", function() {
     it("returns flase when all elements in target array are not in comparison array", function() {
       expect(checkIfDepsCompleted([1,2], [1,2,3])).toBe(false);
     });
 });
 
 
-describe("checker function", function() {
+describe("checkIfDepsCompleted", function() {
     it("returns true when all elements in target array are in comparison array", function() {
       expect(checkIfDepsCompleted([1,2,3], [1,2])).toBe(true);
     });
